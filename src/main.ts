a08bd297370163fd6ea8f624ae12d9b1c7804a9a
Christophe Vimbert <c.vimbert@outlook.fr>
2017-03-10 23:19:27 +0100

    Empty project base

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import {Sprite} from "blip-framework/compiled/core/display/sprite.class";
+import {File} from "blip-framework/compiled/core/files/file.class";
+/**
+ * Created by Christophe on 10/03/2017.
+ */
+var ff:File = new File("ook.png");
---
+++ systemjs.config.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Christophe on 31/01/2017.
+ */
+
+(function (global) {
+    System.config({
+        defaultJSExtensions: true,
+        paths: {
+            'npm:': 'node_modules/'
+        },
+        map: {
+            src: "src",
+            'rxjs': 'npm:rxjs',
+            'blip-framework': 'npm:blip-framework/bundles/blip-framework.umd.js',
+        },
+        packages: {
+            src: {
+                main: '../compiled/main.js',
+                defaultExtension: 'js'
+            },
+            'blip-framework': { defaultExtension: 'js', main: 'bundles/blip-framework.umd' },
+        }
+    })
+})(this);
